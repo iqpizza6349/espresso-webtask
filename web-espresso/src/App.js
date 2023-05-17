1c40045e4308c63216e57349dba135b650a8cbbb
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-17 15:56:31 +0900

    :construction: :: progress on score components and add routes each uri

--- web-espresso/src/App.js
+++ web-espresso/src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
-import Score from './component/Score';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const env = process.env;
-env.PUBLIC_URL = env.PUBLIC_URL || "";
+import './App.css';
+import ScoreComponent from './components/score/ScoreComponent';
 
 function App() {
   return (
     <div className="App">
-      <Score text={"sugar"} top={110} imgMargin={100}/>
-      <Score text={"acidity"} top={70} imgMargin={100}/>
-      <Score text={"bitter"} top={70} imgMargin={120}/>
-      <div>
-        <button className='no_outline_btn'>
-          <img src={process.env.PUBLIC_URL + `/images/submit_button.png`}/>
-        </button>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ScoreComponent/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
