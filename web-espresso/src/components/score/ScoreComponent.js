2d83ccaf5119f2c714a4994bdadbae89f6f4e684
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-18 20:22:41 +0900

    :art: :: add axios request, recommands button

--- web-espresso/src/components/score/ScoreComponent.js
+++ web-espresso/src/components/score/ScoreComponent.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Score from './component/Score';
 import Age from './component/Age';
+import { requestGetMethod } from '../../utils/AxiosUtil';
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
@@ -11,6 +12,11 @@ const ScoreComponent = () => {
     const [acidity, setAcidity] = useState(0);
     const [bitter, setBitter] = useState(0);
 
+    const recommandsCoffee = () => {
+        requestGetMethod(`/coffee/recommand?age=${age}&sugar=${sugar}&acidity=${acidity}&bitter=${bitter}`)
+            .then(res => console.log(res));
+    }
+
     return (
         <>
             <Age setAge={setAge}/>
@@ -18,7 +24,7 @@ const ScoreComponent = () => {
             <Score text={"acidity"} top={70} imgMargin={100} setScore={setAcidity}/>
             <Score text={"bitter"} top={70} imgMargin={120} setScore={setBitter}/>
             <div>
-                <button className='no_outline_btn'>
+                <button className='no_outline_btn' onClick={recommandsCoffee}>
                     <img src={process.env.PUBLIC_URL + `/images/submit_button.png`} alt='추천 버튼'/>
                 </button>
             </div>
