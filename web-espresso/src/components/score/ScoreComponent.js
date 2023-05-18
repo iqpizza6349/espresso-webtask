525dd1652ccc64b03624071f7cfc89a54e68d976
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-18 11:32:06 +0900

    :art: :: improve component structure

--- web-espresso/src/components/score/ScoreComponent.js
+++ web-espresso/src/components/score/ScoreComponent.js
@@ -1,19 +1,22 @@
+import { useState } from 'react';
 import Score from './component/Score';
+import Age from './component/Age';
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
 
 const ScoreComponent = () => {
+    const [age, setAge] = useState(-1);
+    const [sugar, setSugar] = useState(0);
+    const [acidity, setAcidity] = useState(0);
+    const [bitter, setBitter] = useState(0);
+
     return (
         <>
-            <div style={{position: 'absolute'} && {marginLeft: 0}}>
-                <img src={`${process.env.PUBLIC_URL}/images/text/age.png`} alt='나이'/>
-                <input type='text' placeholder='insert your age' style={{marginLeft: 190}}/>
-                {/* <img src={`${process.env.PUBLIC_URL}/images/text/input.png`} style={{marginLeft: 190}} alt='label'/> */}
-            </div>
-            <Score text={"sugar"} top={30} imgMargin={100}/>
-            <Score text={"acidity"} top={70} imgMargin={100}/>
-            <Score text={"bitter"} top={70} imgMargin={120}/>
+            <Age setAge={setAge}/>
+            <Score text={"sugar"} top={30} imgMargin={100} setScore={setSugar}/>
+            <Score text={"acidity"} top={70} imgMargin={100} setScore={setAcidity}/>
+            <Score text={"bitter"} top={70} imgMargin={120} setScore={setBitter}/>
             <div>
                 <button className='no_outline_btn'>
                     <img src={process.env.PUBLIC_URL + `/images/submit_button.png`} alt='추천 버튼'/>
--- web-espresso/src/components/score/component/Score.js
+++ web-espresso/src/components/score/component/Score.js
@@ -1,19 +1,31 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 import { ScoreItem } from './ScoreItem';
 
-const Score = ({text, top, imgMargin}) => {
-    // [score, setScore] = useState();
-    
-    const score = ScoreItem.find(it => it.name === text);
-    const item = Array(5).fill(score.item_img.off);
+const Score = ({text, top, imgMargin, setScore}) => {
+    const item = ScoreItem.find(it => it.name === text);
+
+    const [imageSrc, setImageSrc] = useState(Array(5).fill(item.item_img.off));
+
+    const handleClick = (index) => {
+        for (let i = 0; i < index + 1; i++) {
+            imageSrc[i] = item.item_img.on;
+        }
+        for (let i = 4; i > index; i--) {
+            imageSrc[i] = item.item_img.off;
+        }
+        
+        setScore(index + 1);
+        setImageSrc([...imageSrc]);
+    }
 
     return (
         <div style={{position: 'absolute'} && {marginTop: top}}>
-            <img src={score.img} style={{marginLeft: 0}} alt={`${text}`}/>
-            {item.map(function (image) {
+            <img src={item.img} style={{marginLeft: 0}} alt={`${text}`}/>
+            {imageSrc.map(function (value, index) {
                 return (
-                    <img src={image} style={{marginLeft: imgMargin}} alt='점수 이미지'/>
+                    <img src={value} style={{marginLeft: imgMargin}} alt='이미지' key={`${index + text}`} 
+                            onClick={() => handleClick(index)}/>
                 )
             })}
         </div>
