43f1e64070478ac3f499fb91e1babfd159a9dc42
iqpizza6349 <iqpizza6349@gmail.com>
2023-05-19 20:02:06 +0900

    :construction: :: progress recommands coffee navigate logic

--- web-espresso/src/components/score/ScoreComponent.js
+++ web-espresso/src/components/score/ScoreComponent.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Score from './component/Score';
 import Age from './component/Age';
 import { requestGetMethod } from '../../utils/AxiosUtil';
@@ -7,14 +8,22 @@ const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
 
 const ScoreComponent = () => {
+    const navigate = useNavigate();
     const [age, setAge] = useState(-1);
     const [sugar, setSugar] = useState(0);
     const [acidity, setAcidity] = useState(0);
     const [bitter, setBitter] = useState(0);
 
+    // axios 로 가져온 데이터를 sessionStorage 에 저장
+    // 그리고 해당 sessionStorage 키를 param 으로 넘겨줌
     const recommandsCoffee = () => {
         requestGetMethod(`/coffee/recommand?age=${age}&sugar=${sugar}&acidity=${acidity}&bitter=${bitter}`)
-            .then(res => console.log(res));
+            .then(res => {
+                const uuid = window.self.crypto.randomUUID();
+                window.sessionStorage.setItem(uuid, JSON.stringify(res.data));
+                return uuid;
+            })
+            .then(key => navigate(`/recommands?key=${key}`));
     }
 
     return (
